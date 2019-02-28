2d36affacf10ae1c52f280756bc3cd7fa5f9af79
vinsoncho <997462392@qq.com>
2019-02-28 23:32:27 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git config --global user.email 997462392@qq.com &&
 git config --global user.name vinsoncho &&
 git fetch &&
-yarn bulid &&
+yarn build &&
 cp -r .circleci public/.circleci && 
 yarn deploy
