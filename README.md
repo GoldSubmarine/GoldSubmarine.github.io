8a46372a0a29a42cd65409ba02da6ec5a2433d3b
vinsoncho <997462392@qq.com>
2019-03-01 00:05:53 +0800

    test

---
+++ README.md
@@ -0,0 +1,5 @@
+# vinsoncho.github.io
+
+🐌 reading notes
+
+By visiting the URL : [https://vinsoncho.github.io/](https://vinsoncho.github.io/)
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
+export TZ='Asia/Shanghai' &&
 git config --global user.email 997462392@qq.com &&
 git config --global user.name vinsoncho &&
-git fetch &&
+
+
+# 使用已部署文件初始化目标
+git clone --depth 1 --branch=master https://github.com/vinsoncho/vinsoncho.github.io.git .deploy_git
+cd .deploy_git
+find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
+cd ../
+
+# 部署
 yarn build &&
 cp -r .circleci public/.circleci && 
+cp README.md public/README.md &&
 yarn deploy
