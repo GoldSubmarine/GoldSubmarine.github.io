0525711eb45d556766a2c18543144ede30a170d6
vinsoncho <997462392@qq.com>
2019-03-01 00:19:53 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ git config --global user.name vinsoncho &&
 
 # 使用已部署文件初始化目标
 git clone --depth 1 --branch=master https://github.com/vinsoncho/vinsoncho.github.io.git .deploy_git
-cd .deploy_git
-find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
-cd ../
+# cd .deploy_git
+# find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
+# cd ../
 
 # 部署
 yarn build &&
