90f2c40e17cb3a518942fbdb6c3f63f49e3ec5da
vinson <cao.wenjun@htnova.com>
2019-03-01 09:37:08 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ git config --global user.name vinsoncho &&
 
 
 # 使用已部署文件初始化目标
-git clone --depth 1 --branch=master https://github.com/vinsoncho/vinsoncho.github.io.git .deploy_git
-cd .deploy_git
-find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
-cd ../
+git clone --depth 1 --branch=master https://github.com/vinsoncho/vinsoncho.github.io.git .deploy_git &&
+cd .deploy_git &&
+rm -rf !(.git) &&
+cd ../ &&
 
 # 部署
 yarn build &&
