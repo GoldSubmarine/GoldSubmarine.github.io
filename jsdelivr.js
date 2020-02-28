4c1180d33f4f0c38a8777232af80f6fa1cbe555f
charles <997462392@qq.com>
2020-02-28 13:26:53 +0800

    add

--- jsdelivr.js
+++ jsdelivr.js
@@ -5,15 +5,12 @@ const cdn = "https://cdn.jsdelivr.net/gh/goldsubmarine/goldsubmarine.github.io@m
 
 glob("./public/**/*.html", {}, function(er, files) {
     files.forEach(page => {
-        fs.readFile(page, {}, (err, buffer) => {
-            let str = buffer.toString()
-            str = str.replace(/src="\/js\//, `src="${cdn}/js/`)
-            str = str.replace(/src="\/images\//, `src="${cdn}/images/`)
-            str = str.replace(/href="\/css\//, `href="${cdn}/css/`)
-            str = str.replace(/href="\/images\//, `href="${cdn}/images/`)
-            fs.writeFile(page, str, {}, err => {
-                console.log("页面 cdn 成功：", page)
-            })
-        })
+        let buffer = fs.readFileSync(page, {})
+        let str = buffer.toString()
+        str = str.replace(/src="\/js\//, `src="${cdn}/js/`)
+        str = str.replace(/src="\/images\//, `src="${cdn}/images/`)
+        str = str.replace(/href="\/css\//, `href="${cdn}/css/`)
+        str = str.replace(/href="\/images\//, `href="${cdn}/images/`)
+        fs.writeFileSync(page, str, {})
     })
 });
